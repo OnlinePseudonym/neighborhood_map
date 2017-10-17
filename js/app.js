0c730c82e8b630878eb3c98b1625590c751101cf
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-10-16 21:42:29 -0700

    add basic map

---
+++ js/app.js
@@ -0,0 +1,259 @@
+var model = {
+    map: null,
+    // Points of interest in Phoenix
+    // Consider hosting elsewhere if > 15
+    locations: [
+        {title: 'Tempe Town Lake',loc: {lat: 33.4316776 lng: -111.9276565},},
+        {title: 'Desert Botanical Garden', loc: {lat: 33.460598 lng: -111.947776}}
+    ],
+    // Centerpoint for map, currently ASU
+    mapStart: {lat: 33.4242399, lng: -111.9280527},
+    // Custom style from Snazzymap to express the
+    // baron desert wasteland that is Arizona
+    styles: [
+        {
+            "featureType": "landscape.natural",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ecd5c3"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#32c4fe"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape.natural",
+            "stylers": [
+                {
+                    "visibility": "simplified"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "landscape.man_made",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#baaca2"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#565757"
+                },
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#808080"
+                },
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.neighborhood",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.locality",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#535555"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#fffffe"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "on"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "saturation": -100
+                },
+                {
+                    "lightness": 17
+                }
+            ]
+        },
+        {}
+    ]
+}
+
+var viewModel = {
+    // Main initialization
+    init: function() {
+        mapView.init();
+    },
+    getStart: function() {
+        return model.mapStart;
+    },
+    getStyles: function() {
+        return model.styles;
+    },
+    getMap: function() {
+        return model.map;
+    }
+    setMap: function(map) {
+        model.map = map;
+    }
+}
+
+var mapView = {
+    // initialize google map and store to the model
+    init: function() {
+        this.renderMap();
+    },
+    renderMap: function() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: viewModel.getStart(),
+            styles: viewModel.getStyles(),
+            zoom: 11
+        });
+        viewModel.setMap(map)
+    }
+}
